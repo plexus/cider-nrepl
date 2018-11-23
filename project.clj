ac0be6ab31e7d3895622f8ddd8afe5407c8fbb0a
Arne Brasseur <arne@arnebrasseur.net>
2018-11-23 14:31:10 +0100

    Facepalm

--- project.clj
+++ project.clj
@@ -100,7 +100,7 @@
                         :test-paths ["test/clj"]}
              :test-cljs {:test-paths ["test/cljs"]
                          :dependencies [[cider/piggieback "0.3.9"]
-                                        [java.xml.bind/jaxb-api "2.3.1"]]}
+                                        [javax.xml.bind/jaxb-api "2.3.1"]]}
 
              :sysutils {:plugins [[lein-sysutils "0.2.0"]]}
 
